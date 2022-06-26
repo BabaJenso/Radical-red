2d594c2d4677c2c6749a921ebf5b9461d817ea2c
BabaJenso <108221413+BabaJenso@users.noreply.github.com>
2022-06-26 21:16:12 +0200

    Update rr.js

--- js/data/sets/rr.js
+++ js/data/sets/rr.js
@@ -1,5 +1,5 @@
 var SETDEX_SS = {
-"Pidgey":{"Gym Leader Falkner":{"ability":"Keen Eye","level":12,"moves":["Sand Attack","Pluck","Roost","Quick Attack"]}}
-"Hoothoot":{"Gym Leader Falkner":{"ability":"Insomnia","level":15,"moves":["Hypnosis","Pluck","Roost","Foresight"]}}
+"Pidgey":{"Gym Leader Falkner":{"ability":"Keen Eye","level":12,"moves":["Sand Attack","Pluck","Roost","Quick Attack"]}},
+"Hoothoot":{"Gym Leader Falkner":{"ability":"Insomnia","level":15,"moves":["Hypnosis","Pluck","Roost","Foresight"]}},
 "Murkorw":{"Gym Leader Falkner":{"ability":"Insomnia","level":13,"moves":["Leer","Peck","Astonish","No Move"]}}
 }
